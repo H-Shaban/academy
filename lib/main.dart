b10e1e6420b5aa748aa0ea86a4d841008ad9f76d
Hamada <hshaban2010@hotmail.com>
2020-03-29 15:20:11 +0300

    bug in reload page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -24,20 +23,50 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  bool flag=false;
+
+  Widget webView=WebView(
+    initialUrl: 'https://www.google.com/',
+    javascriptMode: JavascriptMode.unrestricted,
+    onWebViewCreated: (WebViewController webViewController){
+      Completer<WebViewController>().complete(webViewController);
+    },
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  AppLifecycleState _notification;
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      webView=WebView(
+        initialUrl: 'https://540academy.com/wp-admin/',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController){
+          Completer<WebViewController>().complete(webViewController);
+        },
+      );
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child:WebView(
-            initialUrl: 'https://540academy.com/wp-admin/',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController){
-              _controller.complete(webViewController);
-            },
-          ),
+          child:webView
       ),
     );
   }
